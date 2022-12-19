b3c759190231e4bf81fab69318f9dd72139b43d3
Elendol <jean.manguy+elendolgithub@gmail.com>
2022-12-19 20:09:03 +0000

    feat: basic functionalities mimicking NCBI's form submissions

---
+++ README.md
@@ -0,0 +1,23 @@
+Lomanai
+=======
+
+## Usage
+
+```sh
+lomanai --species 'Mus musculus' --species 'Homo sapiens'
+#> Mammalia
+#> ++Rodentia
+#> | \-Mus musculus
+#> \+Primates
+#>   \-Homo sapiens
+-----------------------------------
+```
+
+## License
+
+This project is available under the terms of the [Apache 2.0 license](./LICENSE).
+
+
+## Code of Conduct
+
+Please note that this project is released with a [Contributor Code of Conduct](./CODE_OF_CONDUCT.md). By contributing to this project, you agree to abide by its terms.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
+use clap::Parser;
 use miette::{IntoDiagnostic, Result};
 use reqwest::Client;
 
 static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Binomial species name
+    #[arg(short, long)]
+    species: Vec<String>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    let cli = Args::parse();
+
     let client = Client::builder()
         .user_agent(APP_USER_AGENT)
         .gzip(true)
         .build()
         .into_diagnostic()?;
-    let form = reqwest::multipart::Form::new()
-        .text("an", "Bos taurus")
-        .text("an", "Homo sapiens")
-        .text("an", "Mus musculus")
-        .text("cmd", "Save as");
+
+    let mut form = reqwest::multipart::Form::new().text("cmd", "Save as");
+
+    for species in cli.species {
+        form = form.text("an", species);
+    }
 
     let response = client
         .post("https://www.ncbi.nlm.nih.gov/Taxonomy/CommonTree/wwwcmt.cgi")
